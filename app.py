01c93b1ed37a335704096a408a225b03546a09ca
zoyazubairi <zoyazubairi04@gmail.com>
2026-08-16 02:20:24 +0600

    displaying product data frpm database

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 mydb = mysql.connector.connect(host="localhost", user="root", password="", database="wear_unique")
-mydbcursor = mydb.cursor(dictionary=True)
+mycursor = mydb.cursor(dictionary=True)
 
 @app.get("/product/{id}")
 async def product_page(request: Request, id: str):
-    return templates.TemplateResponse(request, "product.html")
+    mycursor.execute("SHOW * FROM products WHERE id = %s", (id,))
+        
+    product = mycursor.fetchnone()
+    return templates.TemplateResponse(request, "product.html", {
+        "product": product})
+
 
